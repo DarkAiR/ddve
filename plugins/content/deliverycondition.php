13b0645ab10491070462c703843134bcca59c21e
Dmitry Romanov <darkair2@gmail.com>
2013-12-07 15:27:11 +0600

    output delivery condition

--- plugins/content/deliverycondition.php
+++ plugins/content/deliverycondition.php
@@ -11,6 +11,13 @@ $mainframe->registerEvent( 'onPrepareContent', 'plgContentDeliveryCondition' );
 function plgContentDeliveryCondition( &$row, &$params, $page=0 )
 {
     $pattern = "/\{plugin:deliveryCondition\}/i";
-    $icon = 'HelloWorld';
-    $row->text = preg_replace($pattern, $icon, $row->text);
+
+    $db =& JFactory::getDBO();
+    $query = 'SELECT * FROM #__delivery_condition WHERE published=1 ORDER BY ordering';
+    $db->setQuery($query);
+    $items = $db->loadObjectList();
+    $str = '';
+    foreach ($items as $item)
+        $str .= $item['text'].' - '.$item['summ'].'<br/>';
+    $row->text = preg_replace($pattern, $str, $row->text);
 }
