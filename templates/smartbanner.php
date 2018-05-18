0b997c2f75120e177ba579701c924beedcaa2898
DarkAiR <darkair@list.ru>
2018-05-18 18:41:09 +0500

    mobile

--- templates/smartbanner.php
+++ templates/smartbanner.php
@@ -1,23 +1,13 @@
-<!-- smartbanner -->
-<meta name="smartbanner:title" content="ДайтеДве!">
-<meta name="smartbanner:author" content="RuBeacon">
-<meta name="smartbanner:price" content="Бесплатно">
-
-<meta name="smartbanner:price-suffix-apple" content=" ">
-<meta name="smartbanner:price-suffix-google" content=" ">
-
-<!-- Иконки -->
-<meta name="smartbanner:icon-apple" content="http://lh3.googleusercontent.com/KF__HAG6kTuhWd9aZLpP3Y6uUgfa-Wz2gxd1WMkk08i0Qx9M0UHoBiQsUReIDZey9NrrpcWuSIWq96RL_PSo4jJ_E7XTkTeNhBeVZ2TgzJpqcM0=s960">
-<meta name="smartbanner:icon-google" content="http://lh3.googleusercontent.com/KF__HAG6kTuhWd9aZLpP3Y6uUgfa-Wz2gxd1WMkk08i0Qx9M0UHoBiQsUReIDZey9NrrpcWuSIWq96RL_PSo4jJ_E7XTkTeNhBeVZ2TgzJpqcM0=s960">
-
-<!-- Текст кнопки -->
-<meta name="smartbanner:button" content="Скачать">
-
-<!-- Ссылки на аппы -->
-<meta name="smartbanner:button-url-apple" content="https://itunes.apple.com/app/1375485545">
-<meta name="smartbanner:button-url-google" content="https://play.google.com/store/apps/details?id=com.rubeacon.daitedve">
-
-<meta name="smartbanner:enabled-platforms" content="android,ios">
-<link rel="stylesheet" href="./dve_files/smartbanner.min.css">
-<script src="./dve_files/smartbanner.min.js"></script>
-<!-- smartbanner -->
+<script>
+    function getCookie(name) {
+        var matches = document.cookie.match(new RegExp(
+            "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+        ));
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+    }
+    
+    if (/Android|webOS|iPhone|iPad|iPod|IEMobile|Opera Mini/i.test(navigator.userAgent) && getCookie('viewedMobile') != 1) {
+        document.cookie = "viewedMobile=1";
+        window.location.replace("http://ddve.ru/mobile.html");
+    }
+</script>
