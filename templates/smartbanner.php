7d560060d034eb164da81559dde534ac3ecf4b5c
DarkAiR <darkair2@gmail.com>
2018-05-28 22:43:35 +0500

    smartbanner

--- templates/smartbanner.php
+++ templates/smartbanner.php
@@ -1,13 +1,14 @@
-<script>
-    function getCookie(name) {
-        var matches = document.cookie.match(new RegExp(
-            "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-        ));
-        return matches ? decodeURIComponent(matches[1]) : undefined;
-    }
-    
-    if (/Android|webOS|iPhone|iPad|iPod|IEMobile|Opera Mini/i.test(navigator.userAgent) && getCookie('viewedMobile') != 1) {
-        document.cookie = "viewedMobile=1";
-        window.location.replace("http://ddve.ru/mobile.html");
-    }
-</script>
+<meta name="smartbanner:title" content="ДайтеДве!">
+<meta name="smartbanner:author" content="RuBeacon">
+<meta name="smartbanner:price" content="Бесплатно">
+<meta name="smartbanner:price-suffix-apple" content=" ">
+<meta name="smartbanner:price-suffix-google" content=" ">
+<meta name="smartbanner:icon-apple" content="http://lh3.googleusercontent.com/2v6Ta_Sov3T1UuQdA5778QD8phMPiJG7P0opUDy71lmxbjzpdqnS587Sx6fRHQhg5uMPkk9GkC9bRxkLuKa5PzVBZjifZz4AREpzD_sevwWyoNBW=s512">
+<meta name="smartbanner:icon-google" content="http://lh3.googleusercontent.com/2v6Ta_Sov3T1UuQdA5778QD8phMPiJG7P0opUDy71lmxbjzpdqnS587Sx6fRHQhg5uMPkk9GkC9bRxkLuKa5PzVBZjifZz4AREpzD_sevwWyoNBW=s512">
+<meta name="smartbanner:button" content="Скачать">
+<!-- Ссылки на аппы -->
+<meta name="smartbanner:button-url-apple" content="https://itunes.apple.com/app/1341605379">
+<meta name="smartbanner:button-url-google" content="https://play.google.com/store/apps/details?id=com.rubeacon.daitedve">
+<meta name="smartbanner:enabled-platforms" content="android,ios">
+<link rel="stylesheet" href="http://smartbanner.doubleb-automation-production.appspot.com/static/smartbanner/new/smartbanner.min.css">
+<script src="http://smartbanner.doubleb-automation-production.appspot.com/static/smartbanner/new/smartbanner.min.js"></script>
